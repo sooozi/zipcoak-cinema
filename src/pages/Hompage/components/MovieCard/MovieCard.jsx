dcd9165f0d3a1d6e0fdcac0e01e8aa564989fe17
Suzy Kim <skme1103@naver.com>
2024-09-03 15:06:35 +0900

    Add style

--- src/pages/Hompage/components/MovieCard/MovieCard.jsx
+++ src/pages/Hompage/components/MovieCard/MovieCard.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Badge } from 'react-bootstrap';
+import Stack from 'react-bootstrap/Stack';
 import './MovieCard.style.css';
 
 const MovieCard = ({movie}) => {
@@ -11,9 +12,19 @@ const MovieCard = ({movie}) => {
               {movie.genre_ids.map((id) => {
                   <Badge bg="danger">{id}</Badge>
               })}
-              <div>{movie.vote_average}</div>
-              <div>{movie.popularity}</div>
-              <div>{movie.adult ? "OVER 18" : "UNDER 18"}</div>
+              <Stack className='movie-conts' direction="horizontal" gap={2}>
+                <div className='movie-conts-top'>
+                  <Badge className='movie-cont' pill bg="primary">
+                  ⭐ {movie.vote_average}
+                  </Badge>
+                  <Badge className='movie-cont' pill bg="secondary">
+                  💕 {movie.popularity}
+                  </Badge>
+                </div>
+                <Badge className='movie-cont cont-adult' pill bg="secondary">
+                  📛 {movie.adult ? "OVER 18" : "UNDER 18"}
+                </Badge>
+              </Stack>
             </div>
         </div>
     </div>
--- src/pages/Hompage/components/PopularMovieSlide/PopularMovieSlide.jsx
+++ src/pages/Hompage/components/PopularMovieSlide/PopularMovieSlide.jsx
@@ -9,7 +9,7 @@ import './PopularMovieSlide.style.css';
 const responsive = {
     desktop: {
       breakpoint: { max: 3000, min: 1024 },
-      items: 6
+      items: 5
     },
     tablet: {
       breakpoint: { max: 1024, min: 464 },
@@ -17,7 +17,7 @@ const responsive = {
     },
     mobile: {
       breakpoint: { max: 464, min: 0 },
-      items: 2
+      items: 1
     }
 };
 
