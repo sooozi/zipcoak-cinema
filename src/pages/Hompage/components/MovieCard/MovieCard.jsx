eccbdc13a3621003123bfdb2614fef1b4fb80d96
Suzy Kim <skme1103@naver.com>
2024-09-03 15:18:40 +0900

    Add style

--- src/pages/Hompage/components/MovieCard/MovieCard.jsx
+++ src/pages/Hompage/components/MovieCard/MovieCard.jsx
@@ -3,32 +3,39 @@ import { Badge } from 'react-bootstrap';
 import Stack from 'react-bootstrap/Stack';
 import './MovieCard.style.css';
 
-const MovieCard = ({movie}) => {
+const MovieCard = ({ movie }) => {
   return (
     <div className='movie-card-wrap'>
-        <div className='movie-card-box' style={{backgroundImage:`url(https://media.themoviedb.org/t/p/w300_and_h450_bestv2${movie.poster_path})`,}}>
-            <div className='movie-card-overlay'>
-              <h5 className='movie-card-tit'>{movie.title}</h5>
-              {movie.genre_ids.map((id) => {
-                  <Badge bg="danger">{id}</Badge>
-              })}
-              <Stack className='movie-conts' direction="horizontal" gap={2}>
-                <div className='movie-conts-top'>
-                  <Badge className='movie-cont' pill bg="primary">
-                  ⭐ {movie.vote_average}
-                  </Badge>
-                  <Badge className='movie-cont' pill bg="secondary">
-                  💕 {movie.popularity}
-                  </Badge>
-                </div>
-                <Badge className='movie-cont cont-adult' pill bg="secondary">
-                  📛 {movie.adult ? "OVER 18" : "UNDER 18"}
-                </Badge>
-              </Stack>
+      <div 
+        className='movie-card-box'
+        style={{
+          backgroundImage: `url(https://media.themoviedb.org/t/p/w300_and_h450_bestv2${movie.poster_path})`,
+        }}
+      >
+        <div className='movie-card-overlay'>
+          <h5 className='movie-card-tit'>{movie.title}</h5>
+          <Stack className='movie-conts' direction="horizontal" gap={2}>
+            <div className='movie-conts-top'>
+              {movie.genre_ids.map((id, index) => (
+                <Badge bg="danger" key={index}>{id}</Badge> // map 함수가 값을 반환하도록 수정
+              ))}
             </div>
+            <div className='movie-conts-top'>
+              <Badge className='movie-cont' pill bg="primary">
+                ⭐ {movie.vote_average}
+              </Badge>
+              <Badge className='movie-cont' pill bg="secondary">
+                💕 {movie.popularity}
+              </Badge>
+            </div>
+            <Badge className='movie-cont cont-adult' pill bg="secondary">
+              📛 {movie.adult ? "OVER 18" : "UNDER 18"}
+            </Badge>
+          </Stack>
         </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default MovieCard
+export default MovieCard;
