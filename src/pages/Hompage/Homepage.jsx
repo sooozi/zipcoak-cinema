9a19c75c1b99349f9e499ccff2b5e1a60304b329
Suzy Kim <skme1103@naver.com>
2024-09-03 09:52:56 +0900

    Add slide, card

--- src/pages/Hompage/Homepage.jsx
+++ src/pages/Hompage/Homepage.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import Banner from './components/Banner/Banner'
+import PopularMovieSlide from './components/PopularMovieSlide/PopularMovieSlide'
 
 const Homepage = () => {
   return (
-    <div className="container-wrap-wide HomePage-wrap">
+    <div className="container-wrap-wide HomePage-area">
       <Banner />
-      Homepage
+      <div className="slides-cont">
+        <PopularMovieSlide />
+      </div>
     </div>
   )
 }
---
+++ src/pages/Hompage/components/MovieCard/MovieCard.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Badge } from 'react-bootstrap';
+import './MovieCard.style.css';
+
+const MovieCard = ({movie}) => {
+  return (
+    <div className='movie-card-wrap'>
+        <div className='movie-card-box' style={{backgroundImage:`url(https://media.themoviedb.org/t/p/w300_and_h450_bestv2${movie.poster_path})`,}}>
+            <div className='movie-card-overlay'>
+              <h3 className='movie-card-tit'>{movie.title}</h3>
+              {movie.genre_ids.map((id) => {
+                  <Badge bg="danger">{id}</Badge>
+              })}
+              <div>{movie.vote_average}</div>
+              <div>{movie.popularity}</div>
+              <div>{movie.adult ? "OVER 18" : "UNDER 18"}</div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default MovieCard
